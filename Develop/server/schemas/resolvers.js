ee165d3c084726e78f0abb8239645fdefe6db36c
Jonathan Yuen <jonzyuen@gmail.com>
2021-11-09 18:51:46 -0800

    double checking

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -21,13 +21,16 @@ const resolvers = {
         const userData = await User.findOne({ _id: context.user._id })
           .select('-__v -password')
           .populate('books');
+
         return userData;
       }
+
       throw new AuthenticationError('You are not logged in!');
     },
 
     books: async (parent, { username }) => {
       const params = username ? { username } : {};
+      
       return Book.find(params);
     },
 
@@ -37,7 +40,62 @@ const resolvers = {
   },
 
   Mutation: {
-    
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user};
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      
+      return { token, user };
+    },
+
+    addBook : async (parent, args, context) => {
+      if (context.user) {
+        const book = await Book.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { books: book.bookId } },
+          { new: true }
+        )
+
+        return book;
+      }
+
+      throw new AuthenticationError('You are not logged in!');
+    },
+
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedBook = await Book.findByIdAndUpdate({ bookId });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { books: bookId } },
+          { new: true }
+        )
+
+        return book;
+      }
+
+      throw new AuthenticationError('You are not logged in!');
+    }
   }
 };
 
